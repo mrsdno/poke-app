27893728e1160bdbdd23de3a4fd7d69565765136
AnthonyR <arankin789@gmail.com>
2022-08-02 14:35:32 -0400

    addUser, login are now functional(using graphQl) |queries for me, users, and user all function | used JWT for authentication

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,11 +5,63 @@ const { User, Pokemon, Team } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
-    // queries and mutations go here
+    // queries go here
     Query: {
-        helloTeam: () => {
-            return 'TEAM POKEMON!'
-        }       
+        // queries Logged in User
+        me: async (parent, args, context) => {
+            if(context.user){
+                const userData = await User.findOne({ _id: context.user._id})
+                    .select('-__v -password')
+                    // .populate('pokeTeams')
+
+                return userData;
+            }
+
+            throw new AuthenticationError('Not Logged In');
+        },
+
+        // query all users
+        users: async () => {
+            return User.find()
+                .select('-__v')
+                // .populate('pokeTeams')
+        },
+
+        // query a single user by username
+        user: async (parent, { username }) => {
+            return User.findOne({ username })
+                .select('-__v')
+                // .populate('pokeTeams')
+        }
+
+    },
+
+    // mutations go here
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return {token, user};            
+        },
+
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if(!user){
+                throw new AuthenticationError('Incorrect Credentials');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if(!correctPw){
+                throw new AuthenticationError('Incorrect Credentials');
+            }
+
+            const token = signToken(user);
+
+            return { token, user };
+        },
     }
 };
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,9 +2,33 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
 
-    type Query {
-        helloTeam: String
+    type User {
+        _id: ID
+        username: String
+        email: String
+        teamCount: Int     
+        # need to add Teams: [Team]   
     }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        me: User
+        users: [User]
+        user(username: String!): User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+    }
+
+    
+
+
 `;
 
 module.exports = typeDefs;
