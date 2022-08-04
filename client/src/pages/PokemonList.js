e231f0d925329a992311fba90d496b797a73923a
mrsdno <christine@dno.dev>
2022-08-03 23:17:05 -0400

    query color by drop down

--- client/src/pages/PokemonList.js
+++ client/src/pages/PokemonList.js
@@ -8,7 +8,8 @@ function PokemonList() {
   const [isLoaded, setIsLoaded] = useState(false);
 
   // query for searching
-  const [query, setQuery] = useState("")
+  const [queryName, setQueryName] = useState("");
+  const [queryColor, setQueryColor] = useState("");
 
     async function handleGetPokemon() {
       const pokemon = await fetchPokeApi();
@@ -31,33 +32,62 @@ function PokemonList() {
   return (
     <div>
       <div>
-        <input placeholder="Enter Post Title"
-          onChange={event => setQuery(event.target.value)} />
+        <input
+          placeholder="Enter Post Title"
+          onChange={(event) => setQueryName(event.target.value)}
+        />
+        <select name="color" id="color" onChange={(event) => setQueryColor(event.target.value)}>
+          <option value="">Select Color</option>
+          <option value="green">Green</option>
+          <option value="yellow">Yellow</option>
+          <option value="red">Red</option>
+          <option value="blue">Blue</option>
+          <option value="white">White</option>
+          <option value="brown">Brown</option>
+          <option value="purple">Purple</option>
+          <option value="pink">Pink</option>
+          <option value="gray">Gray</option>
+          <option value="black">Black</option>
+        </select>
       </div>
       <div className="wrapper">
-        {pokemonList && pokemonList.filter(pokemon => {
-          if (query === "") {
-            return pokemon;
-          } else if (pokemon.name.toLowerCase().includes(query.toLowerCase())) {
-            return pokemon;
-          }
-        }).map((pokemon) => (
-            <div className="pokemonCard" key={pokemon.id}>
-              <h1>{pokemon.name}</h1>
-              <img src={pokemon.image} />
-              <p>height: {pokemon.height}</p>
-              <p>weight: {pokemon.weight}</p>
-              <ul>
-                {" "}
-                Types:
-                {pokemon?.types?.map((types) => (
-                  <li key={types.slot}>{types.type.name}</li>
-                ))}
-              </ul>
-              <p>{pokemon.flavorText}</p>
-              <p>color: {pokemon.color}</p>
-            </div>
-          ))}
+        {pokemonList &&
+          pokemonList
+            .filter((pokemon) => {
+              if (queryName === "") {
+                return pokemon;
+              } else if (
+                pokemon.name.toLowerCase().includes(queryName.toLowerCase())
+              ) {
+                return pokemon;
+              }
+            })
+          .filter((pokemon) => {
+            if (queryColor === "") {
+              return pokemon;
+            } else if (
+              pokemon.color.toLowerCase().includes(queryColor.toLowerCase())
+            ) {
+              return pokemon;
+            }
+          })
+            .map((pokemon) => (
+              <div className="pokemonCard" key={pokemon.id}>
+                <h1>{pokemon.name}</h1>
+                <img src={pokemon.image} />
+                <p>height: {pokemon.height}</p>
+                <p>weight: {pokemon.weight}</p>
+                <ul>
+                  {" "}
+                  Types:
+                  {pokemon?.types?.map((types) => (
+                    <li key={types.slot}>{types.type.name}</li>
+                  ))}
+                </ul>
+                <p>{pokemon.flavorText}</p>
+                <p>color: {pokemon.color}</p>
+              </div>
+            ))}
       </div>
     </div>
   );
