d91762510e0dbac2f693ab4cb2f8b230c678f1b5
Andrew Kim <Andrew>
2022-08-09 16:42:45 -0400

    morecssstyling

--- client/src/pages/PokemonTeam.js
+++ client/src/pages/PokemonTeam.js
@@ -1,174 +1,160 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import { useQuery, useMutation } from "@apollo/client";
-import { ADD_TEAM } from '../utils/mutations';
+import { ADD_TEAM } from "../utils/mutations";
 import { QUERY_ME } from "../utils/queries";
 
-import './pages.css';
+import "./pages.css";
 
 function PokemonTeam() {
-    const [teamName, setText] = useState("");
+  const [teamName, setText] = useState("");
   const [addTeam, { error }] = useMutation(ADD_TEAM);
-    const { data: userData } = useQuery(QUERY_ME);
+  const { data: userData } = useQuery(QUERY_ME);
+  const [pokemons, setPokemons] = useState([1, 2, 3, 4, 5, 6]);
 
-  
-      const handleChange = (event) => {
-        if (event.target.value.length <= 280) {
-          setText(event.target.value);
-        }
+  const handleChange = (event) => {
+    if (event.target.value.length <= 280) {
+      setText(event.target.value);
+    }
   };
-  
+
   const handleAddTeam = async (event) => {
     event.preventDefault();
-      console.log(userData);
+    console.log(userData);
 
-      try {
-        const { data } = await addTeam({
-          variables: { teamName, userData },
-        });
-      } catch (e) {
-        console.error(e);
-      }
-    };
-  
-  
-  
+    try {
+      const { data } = await addTeam({
+        variables: { teamName, userData },
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
-    <div className='PokemonTeam'>
+    <div className="PokemonTeam">
       <div className="container-v teambox">
-        <form id="add-team" className='form-addteam' onSubmit={handleAddTeam}>
-          <input id="team-name" className="team-name" value={teamName} placeholder="team name" onChange={handleChange}></input>
-        <button className="btn-1" type="submit" id="add-team">Add Team</button>
+        <form id="add-team" className="form-addteam" onSubmit={handleAddTeam}>
+          <input
+            id="team-name"
+            className="team-name"
+            value={teamName}
+            placeholder="team name"
+            onChange={handleChange}
+          ></input>
+          <button className="btn-1" type="submit" id="add-team">
+            Add Team
+          </button>
         </form>
-          <div className="container-h ">
+        <div className="container-h ">
+          {pokemons.map((pokemon, index) => {
+            if (index < 3) {
+              return (
+                <div className="pokemon card">
+                 
+                  <img
+                    className="card-img-top team-image"
+                    src=""
+                    alt={`Card ${index + 1}`}
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title text-uppercase">
+                      name {index + 1}
+                    </h5>
+                    <div className="card-text">
+                      <p>
+                        <b>Height:</b> decimetres
+                      </p>
+                      <p>
+                        <b>Weight:</b> hectograms
+                      </p>
+                      <p>
+                        <b>Type:</b> Dark{" "}
+                      </p>
+                    </div>
+                  </div>
+                  <div className="pokemon-card-buttonContainer button">
+                    <button
+                      className="btn-2-s delete-pokemon"
+                      type="submit"
+                      id="delete-pokemon"
+                      data-id=""
+                    >
+                      Delete Pokemon
+                    </button>
 
-          {/* first pokemon */}
-          <div className="pokemon">
-            if pokemon selected, pokemon name here
-            <img className="card-img-top team-image" src='' alt="Card1"/>
-
-            <div className="card-body">
-                <h5 className="card-title text-uppercase">name 1</h5>
-                <div className="card-text">
-                  <p><b>Height:</b>  decimetres</p>
-                  <p><b>Weight:</b>  hectograms</p>
-                  <p><b>Type:</b> Dark </p>  
+                    <button
+                      className="btn-1-s  add-pokemon"
+                      type="submit"
+                      id="add-pokemon"
+                      onClick={() => {
+                        window.location.assign("/pokemonlist");
+                      }}
+                    >
+                      Choose your pokemon
+                    </button>
+                  </div>
                 </div>
-            </div>
-
-            <button className="btn-2-s delete-pokemon" type="submit" id="delete-pokemon" data-id=''>Delete Pokemon</button>
-            
-            <button className="btn-1-s  add-pokemon" type="submit" id="add-pokemon" onClick={() => { window.location.assign('/pokemonlist') }}>Choose your pokemon</button>
-            
-          </div>
-
-          {/* second pokemon */}
-          <div className="pokemon card">
-            if pokemon selected, pokemon name here
-            <img className="card-top team-image" src='' alt="Card2"/>
-            <div className="card-body">
-              <h5 className="card-title text-uppercase">Name 2</h5>
-              <div className="card-text">
-                  <p><b>Height:</b>  decimetres</p>
-                  <p><b>Weight:</b>  hectograms</p>
-                  <p><b>Type:</b> Dark </p>  
-                </div>
-            </div>
-
-            <button className="btn-2-s delete-pokemon" type="submit" id="delete-pokemon" data-id=''>Delete Pokemon</button>
-            
-            <button className="btn-1-s add-pokemon" type="button" id="add-pokemon" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Choose your pokemon</button>
-            
-          </div>
-
-          {/* third pokemon */}
-          <div className="pokemon card">
-            if pokemon selected, pokemon name here
-            <img className="card-top team-image" src='' alt="Card3"/>
-            <div className="card-body">
-              <h5 className="card-title text-uppercase">pokemon name 3</h5>
-              <div className="card-text">
-                  <p><b>Height:</b>  decimetres</p>
-                  <p><b>Weight:</b>  hectograms</p>
-                  <p><b>Type: Dark</b> </p>  
-                </div>
-            </div>
-
-            <button className="btn-2-s delete-pokemon" type="submit" id="delete-pokemon" data-id=''>Delete Pokemon</button>
-                
-            <button className="btn-1-s add-pokemon" type="button" id="add-pokemon" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Choose your pokemon</button> 
-          </div>
-
+              );
+            }
+          })}
         </div>
 
-        <div className="container-h">
+        <div className="container-h ">
+          {pokemons.map((pokemon, index) => {
+            if (index > 2) {
+              return (
+                <div className="pokemon card">
+                  
+                  <img
+                    className="card-img-top team-image"
+                    src=""
+                    alt={`Card ${index + 1}`}
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title text-uppercase">
+                      name {index + 1}
+                    </h5>
+                    <div className="card-text">
+                      <p>
+                        <b>Height:</b> decimetres
+                      </p>
+                      <p>
+                        <b>Weight:</b> hectograms
+                      </p>
+                      <p>
+                        <b>Type:</b> Dark{" "}
+                      </p>
+                    </div>
+                  </div>
+                  <div className="pokemon-card-buttonContainer button">
+                    <button
+                      className="btn-2-s delete-pokemon"
+                      type="submit"
+                      id="delete-pokemon"
+                      data-id=""
+                    >
+                      Delete Pokemon
+                    </button>
 
-          {/* fourth pokemon */}
-          <div className="pokemon card">
-            if pokemon selected, pokemon name here
-            <img className="card-top team-image" src='' alt="Card4"/>
-
-            <div className="card-body">
-              <h5 className="card-title text-uppercase">pokemon name 4</h5>
-              <div className="card-text">
-                  <p><b>Height:</b>  decimetres</p>
-                  <p><b>Weight:</b>  hectograms</p>
-                  <p><b>Type:</b> Dark </p>  
+                    <button
+                      className="btn-1-s  add-pokemon"
+                      type="submit"
+                      id="add-pokemon"
+                      onClick={() => {
+                        window.location.assign("/pokemonlist");
+                      }}
+                    >
+                      Choose your pokemon
+                    </button>
+                  </div>
                 </div>
-            </div>
-
-            <button className="btn-2-s delete-pokemon" type="submit" id="delete-pokemon" data-id=''>Delete Pokemon</button>
-                  
-            <button className="btn-1-s add-pokemon" type="button" id="add-pokemon" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Choose your pokemon</button>  
-
-          </div>
-
-          {/* fifth pokemon*/}
-          <div className="pokemon card">
-            if pokemon selected, pokemon name here
-            <img className="card-top team-image" src='' alt="Card5"/>
-
-            <div classname="card-body">
-              <h5 className="card-title text-uppercase">pokemon 5 </h5>
-              <div className="card-text">
-                  <p><b>Height:</b>  decimetres</p>
-                  <p><b>Weight:</b>  hectograms</p>
-                  <p><b>Type:</b> Dark </p>  
-                </div>
-            </div>
-
-            <button className="btn-2-s delete-pokemon" type="submit" id="delete-pokemon" data-id=''>Delete Pokemon</button>
-                      
-            <button className="btn-1-s add-pokemon" type="button" id="add-pokemon" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Choose your pokemon</button>
-                  
-          </div>
-              
-
-          {/* pokemon 6 */}
-          <div className="pokemon card">
-            if pokemon selected, pokemon name here
-            <img className="card-top team-image" src='' alt="Card6"/>
-
-            <div className="card-body">
-              <h5 className="card-title text-uppercase">pokemon name 6</h5>
-              <div className="card-text">
-                  <p><b>Height:</b>  decimetres</p>
-                  <p><b>Weight:</b>  hectograms</p>
-                  <p><b>Type:</b> Dark </p>  
-                </div>
-            </div>
-
-            <button className="btn-2-s delete-pokemon" type="submit" id="delete-pokemon" data-id='' >Delete Pokemon</button>
-                  
-            <button className="btn-1-s add-pokemon" type="button" id="add-pokemon" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Choose your pokemon</button>
-                  
-          </div>
+              );
+            }
+          })}
         </div>
-
       </div>
-    
-
     </div>
-  )
+  );
 }
 
 export default PokemonTeam;
