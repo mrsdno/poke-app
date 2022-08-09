afc4422b8fb0165e79ac59587c29d2c3b4a01fd3
Andrew Kim <Andrew>
2022-08-09 03:53:56 -0400

    teambuildercss

--- client/src/pages/PokemonTeam.js
+++ client/src/pages/PokemonTeam.js
@@ -35,8 +35,8 @@ function PokemonTeam() {
   return (
     <div className='PokemonTeam'>
       <div className="container-v teambox">
-        <form id="add-team" onSubmit={handleAddTeam}>
-          <input id="team-name" value={teamName} placeholder="team name" onChange={handleChange}></input>
+        <form id="add-team" className='form-addteam' onSubmit={handleAddTeam}>
+          <input id="team-name" className="team-name" value={teamName} placeholder="team name" onChange={handleChange}></input>
         <button className="btn-1" type="submit" id="add-team">Add Team</button>
         </form>
           <div className="container-h ">
@@ -48,11 +48,11 @@ function PokemonTeam() {
 
             <div className="card-body">
                 <h5 className="card-title text-uppercase">name 1</h5>
-                <p className="card-text">
-                  Height:  decimetres
-                  Weight:  hectograms
-                  Type:  
-                </p>
+                <div className="card-text">
+                  <p><b>Height:</b>  decimetres</p>
+                  <p><b>Weight:</b>  hectograms</p>
+                  <p><b>Type:</b> Dark </p>  
+                </div>
             </div>
 
             <button className="btn-2-s delete-pokemon" type="submit" id="delete-pokemon" data-id=''>Delete Pokemon</button>
@@ -67,11 +67,11 @@ function PokemonTeam() {
             <img className="card-top team-image" src='' alt="Card2"/>
             <div className="card-body">
               <h5 className="card-title text-uppercase">Name 2</h5>
-              <p className="card-text">
-                Height: decimetres
-                Weight:  hectograms
-                Type:  
-              </p>
+              <div className="card-text">
+                  <p><b>Height:</b>  decimetres</p>
+                  <p><b>Weight:</b>  hectograms</p>
+                  <p><b>Type:</b> Dark </p>  
+                </div>
             </div>
 
             <button className="btn-2-s delete-pokemon" type="submit" id="delete-pokemon" data-id=''>Delete Pokemon</button>
@@ -86,11 +86,11 @@ function PokemonTeam() {
             <img className="card-top team-image" src='' alt="Card3"/>
             <div className="card-body">
               <h5 className="card-title text-uppercase">pokemon name 3</h5>
-              <p className="card-text">
-                Height:  decimetres
-                Weight: hectograms
-                Type: 
-              </p>
+              <div className="card-text">
+                  <p><b>Height:</b>  decimetres</p>
+                  <p><b>Weight:</b>  hectograms</p>
+                  <p><b>Type: Dark</b> </p>  
+                </div>
             </div>
 
             <button className="btn-2-s delete-pokemon" type="submit" id="delete-pokemon" data-id=''>Delete Pokemon</button>
@@ -109,11 +109,11 @@ function PokemonTeam() {
 
             <div className="card-body">
               <h5 className="card-title text-uppercase">pokemon name 4</h5>
-              <p className="card-text">
-                Height:  decimetres
-                Weight:  hectograms
-                Type:  
-              </p>
+              <div className="card-text">
+                  <p><b>Height:</b>  decimetres</p>
+                  <p><b>Weight:</b>  hectograms</p>
+                  <p><b>Type:</b> Dark </p>  
+                </div>
             </div>
 
             <button className="btn-2-s delete-pokemon" type="submit" id="delete-pokemon" data-id=''>Delete Pokemon</button>
@@ -129,11 +129,11 @@ function PokemonTeam() {
 
             <div classname="card-body">
               <h5 className="card-title text-uppercase">pokemon 5 </h5>
-              <p className="card-text">
-                Height:  decimetres
-                Weight:  hectograms
-                Type:  
-              </p>
+              <div className="card-text">
+                  <p><b>Height:</b>  decimetres</p>
+                  <p><b>Weight:</b>  hectograms</p>
+                  <p><b>Type:</b> Dark </p>  
+                </div>
             </div>
 
             <button className="btn-2-s delete-pokemon" type="submit" id="delete-pokemon" data-id=''>Delete Pokemon</button>
@@ -150,11 +150,11 @@ function PokemonTeam() {
 
             <div className="card-body">
               <h5 className="card-title text-uppercase">pokemon name 6</h5>
-              <p className="card-text">
-                Height:  decimetres
-                Weight:  hectograms
-                Type: 
-              </p>
+              <div className="card-text">
+                  <p><b>Height:</b>  decimetres</p>
+                  <p><b>Weight:</b>  hectograms</p>
+                  <p><b>Type:</b> Dark </p>  
+                </div>
             </div>
 
             <button className="btn-2-s delete-pokemon" type="submit" id="delete-pokemon" data-id='' >Delete Pokemon</button>
