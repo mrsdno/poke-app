8e5127c583bf80d3acd487c8e854621ce37accf0
mrsdno <christine@dno.dev>
2022-08-10 00:43:37 -0400

    remove .jsx files to deploy

--- client/src/Components/Header/Header.js
+++ client/src/Components/Header/Header.js
@@ -5,35 +5,6 @@ import pokemonLogo from '../../assets/International_Pokémon_logo.svg.png'
 import { Link } from 'react-router-dom';
 import Auth from '../../utils/auth';
 
-// function Header() {
-
-//     const logout = event => {
-//         event.preventDefault();
-//         Auth.logout();
-//     };
-
-
-//   return (
-//     <header className='bg-b'>
-//         <div className="container-v bg-b">
-//             <div>
-//                 <img alt="Pokemon Logo" className="pokemon-logo bg-b" src={pokemonLogo} />
-//             </div>
-//             <div className='text-shadow'>
-//                 <button className='btn-1 marg-5' type="button">
-//                     Login
-//                 </button>
-//                 <button className='btn-2 marg-5' type="button" onClick={logout}>
-//                     Log Out
-//                 </button>
-//             </div>
-            
-//         </div>
-//         <br></br>
-//     </header>
-// \
-//   )
-// }
 
 // Header JSX
 const Header = () => {
