4863d5070a50aa839f8977de3b460becb36c668f
mrsdno <christine@dno.dev>
2022-08-10 00:44:53 -0400

    update header imports

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 
 import "./App.css"
 
-import  Header  from './components/Header'
-import  Footer  from './components/Footer'
+import  Header  from './components/Header/Header'
+import  Footer  from './components/Footer/Footer'
 
 
 import Home from "./pages/Home";
